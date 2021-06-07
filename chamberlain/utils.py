9822189c5c37a1d4c61473476412992e4800c265
Ammar Ahmad <ammar.977@hotmail.com>
2021-06-07 13:57:05 -0400

    chamberlain database endpoints added

--- chamberlain/utils.py
+++ chamberlain/utils.py
@@ -100,3 +100,314 @@ def send_submit(ip, payload):
         response = response.json()
 
     return response
+
+# chamberlain db crud operation helper functions
+
+## workflow
+def insert_workflow(mysql, payload):
+    '''
+        This function inserts workflow in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            req: request payload  - dict
+    '''
+
+    if 'operationName' in payload and 'workflowId' in payload:
+        cursor = mysql.connection.cursor()
+        query = 'INSERT INTO chamberlain.workflows (workflowId,operationName) VALUES (%s,%s)'
+        cursor.execute(query,( payload['workflowId'],payload['operationName'] ))
+        mysql.connection.commit()
+        cursor.close()
+
+        return 'successfull'
+    else:
+        return "request format not correct"
+
+def get_workflows(mysql):
+    '''
+        This function returns workflow in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'SELECT * FROM chamberlain.workflows'
+    cursor.execute(query)
+    data = list(cursor.fetchall())
+    cursor.close()
+
+    return data
+
+def get_workflow_id(mysql,id):
+    '''
+        This function returns workflow with specified in from the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            id: id of workflow - string
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'SELECT * FROM chamberlain.workflows WHERE workflowId="' + str(id) + '"'
+    cursor.execute(query)
+    data = list(cursor.fetchall())
+    cursor.close()
+
+    return data
+
+def delete_workflow(mysql,id):
+    '''
+        This function deletes workflow with specified in from the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            id: id of workflow - string
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'DELETE FROM chamberlain.workflows WHERE workflowId="' + str(id) + '"'
+    cursor.execute(query)
+    mysql.connection.commit()
+    cursor.close()
+
+    return 'successful'
+
+## dataset
+def insert_dataset(mysql, payload):
+    '''
+        This function inserts dataset in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            req: request payload  - dict
+    '''
+
+    if 'datasetSchema' in payload and 'datasetId' in payload:
+        cursor = mysql.connection.cursor()
+        query = 'INSERT INTO chamberlain.datasets (datasetId,datasetSchema) VALUES (%s,%s)'
+        cursor.execute(query,( payload['datasetId'],payload['datasetSchema'] ))
+        mysql.connection.commit()
+        cursor.close()
+
+        return 'successfull'
+    else:
+        return "request format not correct"
+    
+def get_datasets(mysql):
+    '''
+        This function returns dataset in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'SELECT * FROM chamberlain.datasets'
+    cursor.execute(query)
+    data = list(cursor.fetchall())
+    cursor.close()
+
+    return data
+
+def get_dataset_id(mysql,id):
+    '''
+        This function returns dataset with specified in from the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            id: id of dataset - string
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'SELECT * FROM chamberlain.datasets WHERE datasetId="' + str(id) + '"'
+    cursor.execute(query)
+    data = list(cursor.fetchall())
+    cursor.close()
+
+    return data
+
+def delete_dataset(mysql,id):
+    '''
+        This function deletes dataset with specified in from the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            id: id of dataset - string
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'DELETE FROM chamberlain.datasets WHERE datasetId="' + str(id) + '"'
+    cursor.execute(query)
+    mysql.connection.commit()
+    cursor.close()
+
+    return 'successful'
+
+def modify_dataset(mysql, payload):
+
+    '''
+        This function modifies dataset in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            req: request payload  - dict
+    '''
+
+    if 'datasetSchema' in payload and 'datasetId' in payload:
+        cursor = mysql.connection.cursor()
+        query = 'UPDATE chamberlain.datasets SET datasetSchema =%s WHERE datasetId=%s'
+        cursor.execute(query,( payload['datasetSchema'],payload['datasetId'] ))
+        mysql.connection.commit()
+        cursor.close()
+
+        return 'successfull'
+    
+    else:
+        return "request format not correct"
+
+## cardinal
+def insert_cardinal(mysql, payload):
+    '''
+        This function inserts cardinal in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            req: request payload  - dict
+    '''
+
+    if 'cardinalIp' in payload and 'cardinalId' in payload:
+        cursor = mysql.connection.cursor()
+        query = 'INSERT INTO chamberlain.cardinals (cardinalId,cardinalIp) VALUES (%s,%s)'
+        cursor.execute(query,( payload['cardinalId'],payload['cardinalIp'] ))
+        mysql.connection.commit()
+        cursor.close()
+
+        return 'successfull'
+    else:
+        return "request format not correct"
+    
+def get_cardinals(mysql):
+    '''
+        This function returns cardinal in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'SELECT * FROM chamberlain.cardinals'
+    cursor.execute(query)
+    data = list(cursor.fetchall())
+    cursor.close()
+
+    return data
+
+def get_cardinal_id(mysql,id):
+    '''
+        This function returns cardinal with specified in from the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            id: id of cardinal - string
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'SELECT * FROM chamberlain.cardinals WHERE cardinalId="' + str(id) + '"'
+    cursor.execute(query)
+    data = list(cursor.fetchall())
+    cursor.close()
+
+    return data
+
+def delete_cardinal(mysql,id):
+    '''
+        This function deletes cardinal with specified in from the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            id: id of cardinal - string
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'DELETE FROM chamberlain.cardinals WHERE cardinalId="' + str(id) + '"'
+    cursor.execute(query)
+    mysql.connection.commit()
+    cursor.close()
+
+    return 'successful'
+
+def modify_cardinal(mysql, payload):
+    '''
+        This function modifies cardinal in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            req: request payload  - dict
+    '''
+
+    if 'cardinalIp' in payload and 'cardinalId' in payload:
+        cursor = mysql.connection.cursor()
+        query = 'UPDATE chamberlain.cardinals SET cardinalIp =%s WHERE cardinalId=%s'
+        cursor.execute(query,( payload['cardinalIp'],payload['cardinalId'] ))
+        mysql.connection.commit()
+        cursor.close()
+
+        return 'successfull'
+    
+    else:
+        return "request format not correct"
+
+## workflow relationship
+def insert_workflow_relationship(mysql, payload):
+    '''
+        This function inserts workflow relationship in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            req: request payload  - dict
+    '''
+
+    if 'workflowRelationshipId' in payload and 'datasetId' in payload and 'workflowId' in payload:
+        cursor = mysql.connection.cursor()
+        query = 'INSERT INTO chamberlain.workflowrelationships (workflowRelationshipId,datasetId,workflowId) VALUES (%s,%s,%s)'
+        cursor.execute(query,( payload['workflowRelationshipId'],payload['datasetId'],payload['workflowId'] ))
+        mysql.connection.commit()
+        cursor.close()
+
+        return 'successfull'
+    else:
+        return "request format not correct"
+    
+def get_workflow_relationships(mysql):
+    '''
+        This function returns workflow_relationship in the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'SELECT * FROM chamberlain.workflowrelationships'
+    cursor.execute(query)
+    data = list(cursor.fetchall())
+    cursor.close()
+
+    return data
+
+def get_workflow_relationship_id(mysql,id):
+    '''
+        This function returns workflow_relationship with specified in from the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            id: id of cardinal - string
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'SELECT * FROM chamberlain.workflowrelationships WHERE workflowRelationshipId="' + str(id) + '"'
+    cursor.execute(query)
+    data = list(cursor.fetchall())
+    cursor.close()
+
+    return data
+
+def delete_workflow_relationship(mysql,id):
+    '''
+        This function deletes workflow_relationship with specified in from the MySQL chamberlain database
+        params:
+            mysql: Database connection object
+            id: id of cardinal - string
+    '''
+
+    cursor = mysql.connection.cursor()
+    query = 'DELETE FROM chamberlain.workflowrelationships WHERE workflowRelationshipId="' + str(id) + '"'
+    cursor.execute(query)
+    mysql.connection.commit()
+    cursor.close()
+
+    return 'successful'
--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,9 @@ from flask import request
 from flask import jsonify
 from flask_cors import CORS
 
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+
 from chamberlain.mockDB import DB
 from chamberlain.templates import *
 from chamberlain.utils import *
@@ -13,6 +16,15 @@ cardinal_DB = DB()
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+# set config for database
+load_dotenv()
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT'))
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+# initialize mysql client
+mysql = MySQL(app)
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
@@ -64,6 +76,407 @@ def submit():
         return show_computation_form()
 
 
+# Chamberlain dataset CRUD endpoints
+
+## Workflow endpoints
+@app.route("/api/workflow", methods=["POST","GET"])
+def handle_workflow_req():
+
+    # handle the POST request
+    if request.method == 'POST':
+        """
+        Request format:
+        {
+            "worklowId":"WK103",
+            "operationName": "SUM"
+        }
+        """
+        try:
+            request_data = request.get_json()
+            response_msg = insert_workflow(mysql,request_data)
+            response = {
+                "MSG": response_msg
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+    # otherwise handle the GET request
+    elif request.method == "GET":
+        try:
+            workflows = get_workflows(mysql)
+            response = {
+                "workflows":workflows
+            } 
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+@app.route("/api/workflow/<id>", methods=["GET","DELETE"])
+def handle_workflow_id_req(id):
+
+    # handle the GET request
+    if request.method == 'GET':
+        try:
+            workflow = get_workflow_id(mysql,id)
+            response = {
+                "workflow": workflow
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+    # otherwise handle the DELETE request
+    elif request.method == "DELETE":
+        try:
+            response_msg = delete_workflow(mysql,id)
+            response = {
+                "MSG":response_msg
+            } 
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+## Dataset endpoints
+@app.route("/api/dataset", methods=["POST","GET","PUT"])
+def handle_dataset_req():
+
+    # handle the POST request
+    if request.method == 'POST':
+        """
+            Request format:
+            {
+                "datasetId":"HRI101",
+                "datasetSchema": "age,location,height"
+                "cardinalId":[list of cardinal ids]
+            }
+        """
+        try:
+            request_data = request.get_json()
+            response_msg = insert_dataset(mysql,request_data)
+            response = {
+                "MSG": response_msg
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+    # otherwise handle the GET request
+    elif request.method == "GET":
+        try:
+            datasets = get_datasets(mysql)
+            response = {
+                "datasets":datasets
+            } 
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+    # handle PUT request method
+    elif request.method == 'PUT':
+        """
+            Request format:
+            {
+                "datasetId":"HRI101",
+                "datasetSchema": "age,location,height"
+            }
+        """
+        try:
+            request_data = request.get_json()
+            response_msg = modify_dataset(mysql,request_data)
+            response = {
+                "MSG": response_msg
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+@app.route("/api/dataset/<id>", methods=["GET","DELETE"])
+def handle_dataset_id_req(id):
+
+    # handle the GET request
+    if request.method == 'GET':
+        try:
+            dataset = get_dataset_id(mysql,id)
+            response = {
+                "dataset": dataset
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+    # otherwise handle the DELETE request
+    elif request.method == "DELETE":
+        try:
+            response_msg = delete_dataset(mysql,id)
+            response = {
+                "MSG":response_msg
+            } 
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+## Cardinal endpoints
+@app.route("/api/cardinal", methods=["POST","GET","PUT"])
+def handle_cardinal_req():
+
+    # handle the POST request
+    if request.method == 'POST':
+        """
+            Request format:
+            {
+                "cardinalId":"cardinal023",
+                "cardinalIp": "12.23.34.45"
+            }
+        """
+        try:
+            request_data = request.get_json()
+            response_msg = insert_cardinal(mysql,request_data)
+            response = {
+                "MSG": response_msg
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+    # otherwise handle the GET request
+    elif request.method == "GET":
+        try:
+            cardinals = get_cardinals(mysql)
+            response = {
+                "cardinals":cardinals
+            } 
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+    # handle PUT request method
+    elif request.method == 'PUT':
+        """
+            Request format:
+            {
+                "cardinalId":"cardinal023",
+                "cardinalIp": "12.23.34.45"
+            }
+        """
+        try:
+            request_data = request.get_json()
+            response_msg = modify_cardinal(mysql,request_data)
+            response = {
+                "MSG": response_msg
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+@app.route("/api/cardinal/<id>", methods=["GET","DELETE"])
+def handle_cardinal_id_req(id):
+
+    # handle the GET request
+    if request.method == 'GET':
+        try:
+            cardinal = get_cardinal_id(mysql,id)
+            response = {
+                "cardinal": cardinal
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+    # otherwise handle the DELETE request
+    elif request.method == "DELETE":
+        try:
+            response_msg = delete_cardinal(mysql,id)
+            response = {
+                "MSG":response_msg
+            } 
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+## Workflow Relationship endpoints
+@app.route("/api/workflow-relationship", methods=["POST","GET"])
+def handle_workflow_relationship_req():
+
+    # handle the POST request
+    if request.method == 'POST':
+        """
+            Request format:
+            {
+                "workflowRelationshipId":"WR109",
+                "datasetId": "HRI007",
+                "workflowId":"WK103"
+            }
+        """
+        try:
+            request_data = request.get_json()
+            response_msg = insert_workflow_relationship(mysql,request_data)
+            response = {
+                "MSG": response_msg
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+    # otherwise handle the GET request
+    elif request.method == "GET":
+        try:
+            workflow_relationships = get_workflow_relationships(mysql)
+            response = {
+                "workflow_relationships":workflow_relationships
+            } 
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+
+@app.route("/api/workflow-relationship/<id>", methods=["GET","DELETE"])
+def handle_workflow_relationship_id_req(id):
+
+    # handle the GET request
+    if request.method == 'GET':
+        try:
+            workflow_relationship = get_workflow_relationship_id(mysql,id)
+            response = {
+                "workflow_relationship": workflow_relationship
+            }
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+    # otherwise handle the DELETE request
+    elif request.method == "DELETE":
+        try:
+            response_msg = delete_workflow_relationship(mysql,id)
+            response = {
+                "MSG":response_msg
+            } 
+            
+        except Exception as e:
+            print(e)
+            response = {
+                "ERR": e
+            }
+            app.logger.error(f"Error sending request: {e}")
+            return jsonify(response)
+
+        return jsonify(response)
+
+
 if __name__ != "__main__":
 
     gunicorn_logger = logging.getLogger("gunicorn.error")
@@ -74,4 +487,5 @@ if __name__ != "__main__":
 if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    ## TODO Change the host ip back to 0.0.0.0 before committing
+    app.run(host="127.0.0.1", port=port, debug=True)
