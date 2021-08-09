cb6cbd390316acbb69a455e4424f36719982c00c
Vidya Akavoor <vidyaap@bu.edu>
2021-08-09 12:18:20 -0400

    Add Ionic sidebar start template as client

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'datasets',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'compute',
+    loadChildren: () => import('./compute/compute.module').then( m => m.ComputePageModule)
+  },
+  {
+    path: 'datasets',
+    loadChildren: () => import('./datasets/datasets.module').then( m => m.DatasetsPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Datasets', url: '/datasets', icon: 'analytics' },
+    { title: 'Workflows', url: '/folder/Workflows', icon: 'reader' },
+    { title: 'Cardinals', url: '/folder/Cardinals', icon: 'cloud' },
+    { title: 'Compute', url: '/compute', icon: 'calculator' },
+  ];
+  constructor() {}
+}
